dbbf158707342479975281298a99ee9af1535176
peayah <82358003+peayah@users.noreply.github.com>
2022-07-03 21:27:16 -0700

    add detail css

--- components/meets/MeetDetail.js
+++ components/meets/MeetDetail.js
@@ -1,16 +1,15 @@
-import { process_params } from "express/lib/router";
-import { Fragment } from "react";
+import classes from './MeetDetail.module.css';
 
 function MeetDetail(props) {
     return (
-        <Fragment>
+        <section className={classes.detail}>
             <img 
             src={props.image}
             alt={props.title}/>
             <h1>{props.title}</h1>
             <address>{props.address}</address>
             <p>{props.description}</p>
-        </Fragment>
+        </section>
     );
 }
 
