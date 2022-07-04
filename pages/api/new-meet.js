14a624a18cec1ecbcaf953ab6b8dd0ca2171416a
peayah <82358003+peayah@users.noreply.github.com>
2022-07-04 00:23:31 -0700

    set up api post req

--- pages/api/new-meet.js
+++ pages/api/new-meet.js
@@ -1,1 +1,23 @@
 import { MongoClient} from "mongodb"
+
+async function handler(req, res) {
+    if(req.method === "POST") {
+        const data = req.body;
+
+        const client = MongoClient.connect("mongodb+srv://venlig:taleromto382BWQ@cluster0.hq9gh.mongodb.net/?retryWrites=true&w=majority")
+    
+        const db =  client.db();
+
+        const meetCollection = db.collection("meets");
+
+        const result = await meetCollection.insertOne(data);
+    
+        console.log(result)
+
+        client.close()
+
+        res.status(201).json({message: "Meet inserted"}) ;
+    }
+}
+
+export default handler;
