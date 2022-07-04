3f65b3723cce4fc06629e3ccff95438096712cc2
peayah <82358003+peayah@users.noreply.github.com>
2022-07-03 19:23:38 -0700

    add page components

---
+++ components/meets/MeetList.js
@@ -0,0 +1,20 @@
+import MeetItem from './MeetItem';
+import classes from './MeetList.module.css';
+
+function MeetList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meets.map((meet) => (
+        <MeetItem
+          key={meet.id}
+          id={meet.id}
+          image={meet.image}
+          title={meet.title}
+          address={meet.address}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default MeetList;
